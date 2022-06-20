a864f30d129e5cbe7532f2db139f1c3a180743e5
Mianxiong <757134184@qq.com>
2022-06-20 11:37:45 +0800

    cookie&session

--- server.js
+++ server.js
@@ -22,7 +22,65 @@ var server = http.createServer(function (request, response) {
     /******** 从这里开始看，上面不要看 ************/
 
     console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-    if (path === '/register' && method === 'POST') {
+    const session = JSON.parse(fs.readFileSync('./session.json').toString())
+
+    if (path === "/sign_in" && method === "POST") {
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
+        const array = []
+        request.on('data', (chunk) => {
+            console.log('chunk', chunk)
+            array.push(chunk)
+        })
+        request.on('end', () => {
+            const string = Buffer.concat(array).toString()
+            // console.log('stringBuffer',string)//stringBuffer {"name":"5","password":"5"}
+            const obj = JSON.parse(string)
+            const user = userArray.find((user) => user.name === obj.name && user.password === obj.password)
+            if (user === undefined) {
+                response.statusCode = 400
+                response.setHeader("Content-Type", "text/json;charset=utf-8");
+                response.end(`{"errorCode":4001}`)
+            } else {
+                response.statusCode = 200
+                const random = Math.random()
+                
+                session[random] = {
+                    user_id: user.id
+                }
+                fs.writeFileSync('./session.json', JSON.stringify(session))
+                response.setHeader("Set-Cookie", `session_id=${random};HttpOnly`)
+                // response.setHeader('Set-Cookie', `user_id=${user.id};HttpOnly`)
+                response.end()
+            }
+        })
+    } else if (path === "/home.html") {
+        const cookie = request.headers['cookie']
+        let sessionId
+        try {
+            sessionId = cookie.split(';').filter(s => s.indexOf('session_id=') >= 0)[0].split('=')[1]
+        } catch (error) {
+
+        }
+        if (sessionId && session[sessionId]) {
+            const userId = session[sessionId].user_id
+            const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
+            const user = userArray.find(user => user.id === userId)
+            const homeHtml = fs.readFileSync('./public/home.html').toString()
+            let string
+            if (user) {
+                string = homeHtml.replace('{{loginStatus}}', '已登录').replace('{{user.name}}', user.name)
+            } else {
+                string = homeHtml.replace('{{loginStatus}}', '未登录').replace('{{user.name}}', '')
+            }
+            response.write(string)
+        } else {
+            const homeHtml = fs.readFileSync('./public/home.html').toString()
+            const string = homeHtml.replace('{{loginStatus}}', '未登录').replace('{{user.name}}', '')
+            response.write(string)
+        }
+        response.end()
+    } else if (path === '/register' && method === 'POST') {
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
         const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
         const array = []
